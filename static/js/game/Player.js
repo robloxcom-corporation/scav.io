4988fa3fe6b1185330938ca26a1a20b4fb4aef7d
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2020-04-10 02:44:41 -0700

    made basic player movement (WIP)

---
+++ static/js/game/Player.js
@@ -0,0 +1,55 @@
+const max_vel = 1;
+
+function Player(x, y) {
+    this.posX = x;
+    this.posY = y;
+    this.velX = 0;
+    this.velY = 0;
+    this.angle = 0;
+    
+    this.move = function() {
+        if (pressedKeys['left'] && pressedKeys['up']) {
+            this.velX = -max_vel * Math.sqrt(2)/2;
+            this.velY = max_vel * Math.sqrt(2)/2;
+        } else if (pressedKeys['left'] && pressedKeys['down']) {
+            this.velX = -max_vel * Math.sqrt(2)/2;
+            this.velY = -max_vel * Math.sqrt(2)/2;
+        } else if (pressedKeys['right'] && pressedKeys['up']) {
+            this.velX = max_vel * Math.sqrt(2)/2;
+            this.velY = max_vel * Math.sqrt(2)/2;
+        } else if (pressedKeys['right'] && pressedKeys['down']) {
+            this.velX = max_vel * Math.sqrt(2)/2;
+            this.velY = -max_vel * Math.sqrt(2)/2;
+        } else if (pressedKeys['left']) {
+            this.velX = -max_vel;
+            this.velY = 0;
+        } else if (pressedKeys['right']) {
+            this.velX = max_vel;
+            this.velY = 0;
+        } else if (pressedKeys['up']) {
+            this.velX = 0;
+            this.velY = max_vel;
+        } else if (pressedKeys['down']) {
+            this.velX = 0;
+            this.velY = -max_vel;
+        } else if (!(pressedKeys['left'] || pressedKeys['right'])) {
+            this.velX = 0;
+        } else if (!(pressedKeys['up'] || pressedKeys['down'])) {
+            this.velY = 0;
+        };
+        console.log(pressedKeys);
+
+        this.posX += this.velX;
+        this.posY -= this.velY;
+    };
+
+
+    this.draw = function() {
+        context.beginPath();
+        context.arc(this.posX, this.posY, 10, 0, 2 * Math.PI, false);
+        context.fillStyle = "red";
+        context.fill();
+        context.stroke();
+    };
+
+};
---
+++ static/js/game/main.js
@@ -0,0 +1,70 @@
+const canvas = document.getElementById("gamecanvas");
+const context = canvas.getContext("2d");
+var width;
+var height;
+const keyMap = {
+    39: 'right', // right arrow
+    68: 'right', // d
+    37: 'left', // left arrow
+    65: 'left', // a
+    38: 'up', // up arrow
+    87: 'up', // w
+    40: 'down', // down arrow
+    83: 'down', // s
+};
+
+var pressedKeys;
+var player;
+
+function init() {
+    canvas.width = window.innerWidth - 8;
+    canvas.height = window.innerHeight - 20;
+    width = canvas.width;
+    height = canvas.height;
+
+
+    pressedKeys = {
+        left: false,
+        right: false,
+        up: false,
+        down: false
+    };
+
+
+    player = new Player(500, 500);
+
+    window.addEventListener('keydown', keydown, false);
+    window.addEventListener('keyup', keyup, false);
+};
+
+
+function gameloop(timestamp) {
+
+        context.clearRect(0 ,0 , width, height);
+        player.move();
+        player.draw();
+        
+    window.requestAnimationFrame( gameloop );
+};
+
+
+function keydown(event) {
+    var key = keyMap[event.keyCode];
+    pressedKeys[key] = true;
+    
+    if (key == 'left' && pressedKeys['right']) {
+        pressedKeys['right'] = false;
+    } else if (key == 'right' && pressedKeys['left']) {
+        pressedKeys['left'] = false;
+    } else if (key == 'up' && pressedKeys['down']) {
+        pressedKeys['down'] = false;
+    } else if (key == 'down' && pressedKeys['up']) {
+        pressedKeys['up'] = false;
+    };
+};
+function keyup(event) {
+    var key = keyMap[event.keyCode];
+    pressedKeys[key] = false;
+};
+
+window.onload = function () { init(); gameloop() };
